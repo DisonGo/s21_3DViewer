702cd0e004948a710f9e6e356b19e8a37fe86193
DisonGo <jokerj224@gmail.com>
2023-04-02 15:08:41 +0300

    develop. Added antialising. Corected style path.

--- src/qt_app/src/main.cpp
+++ src/qt_app/src/main.cpp
@@ -8,12 +8,12 @@ int main(int argc, char *argv[])
     QSurfaceFormat format;
     format.setDepthBufferSize(24);
     format.setStencilBufferSize(8);
+    format.setSamples(4);
     format.setVersion(3, 3);
     format.setProfile(QSurfaceFormat::CoreProfile);
     QSurfaceFormat::setDefaultFormat(format);
     QApplication a(argc, argv);
-//    const QString style_file_path = ":/style/style.qss";
-    const QString style_file_path = "/Users/evverenn/Desktop/Projects/Junk/3D_Viewer/src/qt_app/resources/style/style.qss";
+    const QString style_file_path = ":/style/style.qss";
     MainWindow w;
     StyleLoader *style_loader = new StyleLoader(&w, style_file_path);
     style_loader->attach();
