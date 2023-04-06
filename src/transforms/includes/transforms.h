f6bb6bbddc5436df8290f4af16b4161a25e0e23c
burnsdov <fable1336@gmail.com>
2023-04-06 14:59:59 +0300

    develop. Added matrix rotate.

--- src/transforms/includes/transforms.h
+++ src/transforms/includes/transforms.h
@@ -4,8 +4,7 @@
 #include <math.h>
 
 void translate_matrix(float* matrix, float x, float y, float z);
-void rotate_matrix(float* matrix, float angle, float x, float y, float z);
+void rotate_matrix(float* matrix, float angel, float x, float y, float z);
 void scale_matrix(float* matrix, float scale);
-void mul_matrix(float* result, float* a, float* b);
 
 #endif  //  TRANSFORMS_H
