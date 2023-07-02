12bb3db0893f2a51050c3e069e4507a64fde2922
evverenn <jokerj224@gmail.com>
2023-07-02 14:39:21 +0300

    develop. Fixed EObject destructor issue.

--- src/engine/includes/E/EObject.h
+++ src/engine/includes/E/EObject.h
@@ -5,7 +5,7 @@ class EObject {
  public:
   EObjectType type = kNone;
   virtual EObjectType GetType() const { return type; };
-
+  virtual ~EObject() = default;
   bool SameType(const EObject& obj) { return GetType() == obj.GetType(); };
 };
 #endif  // EOBJECT_H
