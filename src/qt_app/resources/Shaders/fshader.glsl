624d1264bee50e4332b9d79e7cc911bece0a262b
evverenn <jokerj224@gmail.com>
2023-07-05 11:32:22 +0300

    develop. Added engine diagram. Flatened fragment color. TODO upd.

--- src/qt_app/resources/Shaders/fshader.glsl
+++ src/qt_app/resources/Shaders/fshader.glsl
@@ -1,6 +1,6 @@
 #version 330 core
-in vec4 f_color;
-//in float pattern_value;
+flat in vec4 f_color;
+// in float pattern_value;
 
 out vec4 FragColor;
 uniform bool roundCircle;
@@ -8,20 +8,23 @@ uniform vec2 dash_pattern;
 uniform bool dashed;
 uniform float line_width;
 void main() {
-    FragColor = f_color;
-    vec2 circCoord = 2.0 * gl_PointCoord - 1.0;
-    if (roundCircle && dot(circCoord, circCoord) > 1.0) {
-        discard;
-    }
-//    if (dashed) {
-//        float width = line_width * (1.0 - 2.0 * abs(mod(pattern_value, dash_pattern.x + dash_pattern.y) - dash_pattern.x) / (dash_pattern.x + dash_pattern.y));
+  FragColor = f_color;
+  vec2 circCoord = 2.0 * gl_PointCoord - 1.0;
+  if (roundCircle && dot(circCoord, circCoord) > 1.0) {
+    discard;
+  }
+  //    if (dashed) {
+  //        float width = line_width * (1.0 - 2.0 * abs(mod(pattern_value,
+  //        dash_pattern.x + dash_pattern.y) - dash_pattern.x) / (dash_pattern.x
+  //        + dash_pattern.y));
 
-//       // Determine if this fragment is part of a dash or a gap
-//        bool is_dash = pattern_value < dash_pattern.x;
-//        if (is_dash) {
-//            FragColor = vec4(f_color.rgb, f_color.a * smoothstep(0.0, 1.0, width));
-//        } else {
-//            FragColor = vec4(0.0, 0.0, 0.0, 0.0);
-//        }
-//    }
+  //       // Determine if this fragment is part of a dash or a gap
+  //        bool is_dash = pattern_value < dash_pattern.x;
+  //        if (is_dash) {
+  //            FragColor = vec4(f_color.rgb, f_color.a * smoothstep(0.0, 1.0,
+  //            width));
+  //        } else {
+  //            FragColor = vec4(0.0, 0.0, 0.0, 0.0);
+  //        }
+  //    }
 }
--- src/qt_app/resources/Shaders/vshader.glsl
+++ src/qt_app/resources/Shaders/vshader.glsl
@@ -1,7 +1,7 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;
+layout(location = 0) in vec3 aPos;
 
-out vec4 f_color;
+flat out vec4 f_color;
 
 uniform vec3 aColor;
 uniform float aPointSize;
@@ -15,18 +15,15 @@ uniform bool dashed;
 uniform vec2 dash_pattern;
 
 out float pattern_value;
-float rnd(float i) {
-        return mod(4000.*sin(23464.345*i+45.345),1.);
-}
-void main()
-{
-    mvp_vec = camMatrix * model * vec4(aPos, 1);
-    gl_Position = mvp_vec;
-    gl_PointSize = 1;
-    f_color = vec4(rnd(aPos.x),rnd(aPos.y),rnd(aPos.z), 1.0);
+float rnd(float i) { return mod(4000. * sin(23464.345 * i + 45.345), 1.); }
+void main() {
+  mvp_vec = camMatrix * model * vec4(aPos, 1);
+  gl_Position = mvp_vec;
+  gl_PointSize = 1;
+  f_color = vec4(rnd(aPos.x), rnd(aPos.y), rnd(aPos.z), 1.0);
 
-    if (dashed) {
-//        float length = length(vec2(fwidth(gl_Position.xy)));
-//        pattern_value = mod(length, dash_pattern.x + dash_pattern.y);
-    }
+  if (dashed) {
+    //        float length = length(vec2(fwidth(gl_Position.xy)));
+    //        pattern_value = mod(length, dash_pattern.x + dash_pattern.y);
+  }
 }
