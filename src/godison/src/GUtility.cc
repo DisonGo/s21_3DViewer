1054539daafd4185230226c6faaa4e59853b1003
DisonGo <jokerj224@gmail.com>
2023-09-22 12:54:23 +0300

    texture. Made godison lib as dependecy projecet.

--- src/godison/src/GUtility.cc
+++
@@ -1,8 +0,0 @@
-#include <godison/GUtility.h>
-
-namespace godison {
-namespace utility {
-float GToRadians(float degrees) { return degrees * M_PI / 180; }
-float GToDegrees(float radians) { return radians * 180 / M_PI; }
-}  // namespace utility
-}  // namespace godison
